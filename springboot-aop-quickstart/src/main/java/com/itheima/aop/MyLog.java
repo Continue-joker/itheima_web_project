23222075258ed6b350e32e6bc0efa224a239bda0
WJ <ladyice@sina.com>
2024-07-10 14:01:36 +0800

    Second commit

---
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/MyLog.java
@@ -0,0 +1,14 @@
+package com.itheima.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解
+ */
+@Retention(RetentionPolicy.RUNTIME)//定义什么时候生效
+@Target(ElementType.METHOD)//作用范围
+public @interface MyLog {
+}
